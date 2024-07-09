0605511cac5cb589606cf22822270fe32cfe4218
VARRTIX <zhao.wei@lebosemi.com>
2024-07-09 11:36:47 +0800

    update: remove keyword 'noexcept' for generator func

--- include/command.hpp
+++ include/command.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <functional>
 #include <string>
 #include <unordered_map>
@@ -27,8 +28,7 @@ public:
                               : completion::make_unique(std::move(items))) {}
 
   inline virtual char *param_generator(const char *text, int state) noexcept {
-    return param_completion_ ? param_completion_->generator(text, state)
-                             : nullptr;
+    return has_param() ? param_completion_->generator(text, state) : nullptr;
   }
 
   inline virtual bool execute(const exec_args &args) const {
@@ -36,6 +36,9 @@ public:
   }
 
   inline const std::string &get_name() const noexcept { return name_; }
+  inline bool has_param() const noexcept {
+    return param_completion_ && *param_completion_;
+  }
 
 protected:
   std::string name_;
@@ -66,6 +69,17 @@ public:
     return false;
   }
 
+  inline basic_completion::generator_func
+  find_param_generator(const std::string &name) {
+    auto it = cmds_map_.find(name);
+    return std::bind(&basic_completion::generator, it->second.get(),
+                     std::placeholders::_1, std::placeholders::_2);
+    // return completion::generator_func(
+    // std::move(std::bind(&completion::generator, it->second.get(),
+    // std::placeholders::_1, std::placeholders::_2)));
+    // return termctl::basic_completion::generator_func(nullptr);
+  }
+
 private:
   map_type cmds_map_;
 };
@@ -73,7 +87,20 @@ private:
 
 namespace termctl {
 inline basic_command::ptr make_exit_command() {
-  return std::make_unique<basic_command>("exit",
-                                         [](const auto &) { return true; });
+  return std::make_unique<basic_command>("exit", [](const auto &) {
+    std::exit(EXIT_SUCCESS);
+    return true;
+  });
+}
+
+inline basic_command::ptr make_get_command(completion::items_type &&items) {
+  return std::make_unique<basic_command>(
+      "get",
+      [](const auto &args) {
+        auto arg = args.empty() ? "empty" : args[0];
+        std::cout << "get param: [" << arg << "]" << std::endl;
+        return true;
+      },
+      std::move(items));
 }
 } // namespace termctl
