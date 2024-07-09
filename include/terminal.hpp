e1617b6415e91f4f99497461e3e19f3ee060d2b4
VARRTIX <zhao.wei@lebosemi.com>
2024-07-09 15:18:45 +0800

    update: terminal.hpp completed

---
+++ include/terminal.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <cstdlib>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <readline/history.h>
+#include <readline/readline.h>
+
+#include "command.hpp"
+#include "completion.hpp"
+
+namespace termctl {
+class terminal final {
+public:
+  terminal(const terminal &) = delete;
+  terminal &operator=(const terminal &) = delete;
+
+  inline static terminal &shared() {
+    static terminal term_;
+    return term_;
+  }
+
+  inline void set_prompt(const std::string &name) { prompt_ = name + "> "; }
+  inline const std::string &prompt() const noexcept { return prompt_; }
+
+  void register_commands(std::vector<basic_command::ptr> &&cmds);
+
+  void run(const std::string &name);
+
+private:
+  terminal() { rl_attempted_completion_function = command_completion; }
+
+  static char **command_completion(const char *text, int start, int end);
+
+  inline static char *generic_generator(const char *text, int state) {
+    return shared().generator_ ? shared().generator_(text, state) : nullptr;
+  }
+
+  inline void assign_cmd_generator() noexcept {
+    generator_ = std::bind(&basic_completion::generator, cmd_completion_.get(),
+                           std::placeholders::_1, std::placeholders::_2);
+  }
+
+  inline void assign_param_generator(const std::string &name) {
+    generator_ = cmds_.find_param_generator(name);
+  }
+
+  inline void reset_generator() { generator_ = nullptr; }
+
+  std::string prompt_;
+  commands cmds_;
+  basic_completion::ptr cmd_completion_;
+  basic_completion::generator_func generator_;
+};
+
+inline char **terminal::command_completion(const char *text, int start,
+                                           int end) {
+  char **matches = nullptr;
+
+  rl_attempted_completion_over = 1;
+  shared().reset_generator();
+  if (start == 0) {
+    shared().assign_cmd_generator();
+  } else {
+    auto cmd = std::string_view(rl_line_buffer, start);
+    auto pos = cmd.find(' ');
+    if (pos != std::string::npos)
+      cmd = cmd.substr(0, pos);
+    shared().assign_param_generator(std::string(std::move(cmd)));
+  }
+
+  if (shared().generator_)
+    matches = rl_completion_matches(text, generic_generator);
+
+  return matches;
+}
+
+inline void
+terminal::register_commands(std::vector<basic_command::ptr> &&cmds) {
+  auto names = completion::items_type();
+  for (auto &cmd : cmds) {
+    names.push_back(cmd->get_name());
+    cmds_.register_command(std::move(cmd));
+  }
+
+  cmd_completion_ = completion::make_unique(std::move(names));
+}
+
+inline void terminal::run(const std::string &name) {
+  std::unique_ptr<char, decltype(&std::free)> input(nullptr, std::free);
+  set_prompt(name);
+  while (true) {
+    input.reset(readline(prompt().c_str()));
+    if (!input)
+      break;
+
+    std::istringstream iss(input.get());
+    std::string cmd, arg;
+    basic_command::exec_args args;
+    iss >> cmd;
+    while (iss >> arg)
+      args.push_back(std::move(arg));
+
+    if (!cmd.empty()) {
+      add_history(cmd.c_str());
+      cmds_.execute_command(cmd, std::move(args));
+    }
+  }
+}
+} // namespace termctl
