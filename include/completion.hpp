b7bccaa0ada0f4e1697b7d2eec34b50bd36be1f3
VARRTIX <zhao.wei@lebosemi.com>
2024-07-09 13:12:28 +0800

    update: remove noexcept on generator; add operator bool overloading for basic_completion

--- include/completion.hpp
+++ include/completion.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cstring>
-#include <iostream>
+#include <functional>
 #include <memory>
 #include <string>
 #include <vector>
@@ -11,6 +11,7 @@ class basic_completion {
 public:
   using ptr = std::unique_ptr<basic_completion>;
   using items_type = std::vector<std::string>;
+  using generator_func = std::function<char *(const char *, int)>;
 
   basic_completion() = delete;
   basic_completion(basic_completion &&) noexcept = default;
@@ -22,7 +23,9 @@ public:
   explicit basic_completion(items_type &&items) noexcept
       : items_(std::move(items)), iter_(items_.cend()) {}
 
-  virtual char *generator(const char *text, int state) noexcept = 0;
+  explicit operator bool() const noexcept { return !items_.empty(); }
+
+  virtual char *generator(const char *text, int state) = 0;
 
 protected:
   items_type items_;
@@ -37,27 +40,21 @@ public:
     return std::make_unique<completion>(std::move(items));
   }
 
-  char *generator(const char *text, int state) noexcept override;
+  char *generator(const char *text, int state) override;
 };
 
-inline char *completion::generator(const char *text, int state) noexcept {
-  try {
-    if (state == 0)
-      iter_ = items_.cbegin();
+inline char *completion::generator(const char *text, int state) {
+  if (state == 0)
+    iter_ = items_.cbegin();
 
-    const auto len = std::strlen(text);
-    if (len)
-      while (iter_ != items_.cend()) {
-        const auto &item = *iter_;
-        ++iter_;
-        if (item.compare(0, len, text) == 0)
-          return strdup(item.c_str());
-      }
-  } catch (const std::exception &e) {
-    std::cerr << "exception caught: " << e.what() << std::endl;
-  } catch (...) {
-    std::cerr << "unknown exception caught." << std::endl;
-  }
+  const auto len = std::strlen(text);
+  if (len)
+    while (iter_ != items_.cend()) {
+      const auto &item = *iter_;
+      ++iter_;
+      if (item.compare(0, len, text) == 0)
+        return strdup(item.c_str());
+    }
 
   return nullptr;
 }
