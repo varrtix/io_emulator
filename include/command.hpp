92d1ba712b55d03d1e6d6eacee1ac973bfbca803
VARRTIX <zhao.wei@lebosemi.com>
2024-07-11 14:49:15 +0800

    remove: inline keyword from member func on class

--- include/command.hpp
+++ include/command.hpp
@@ -33,16 +33,16 @@ public:
                               ? nullptr
                               : completion::make_unique(std::move(items))) {}
 
-  inline virtual char *param_generator(const char *text, int state) noexcept {
+  virtual char *param_generator(const char *text, int state) noexcept {
     return has_param() ? param_completion_->generator(text, state) : nullptr;
   }
 
-  inline virtual bool execute(exec_args &&args) const {
+  virtual bool execute(exec_args &&args) const {
     return exec_ ? exec_(args) : false;
   }
 
-  inline const std::string &get_name() const noexcept { return name_; }
-  inline bool has_param() const noexcept {
+  const std::string &get_name() const noexcept { return name_; }
+  bool has_param() const noexcept {
     return param_completion_ && *param_completion_;
   }
 
@@ -63,18 +63,18 @@ public:
   commands &operator=(commands &&) noexcept = default;
   ~commands() = default;
 
-  inline void register_command(basic_command::ptr &&cmd) {
+  void register_command(basic_command::ptr &&cmd) {
     cmds_map_.emplace(cmd->get_name(), std::move(cmd));
   }
 
-  inline virtual bool execute_command(const std::string &name,
-                                      basic_command::exec_args &&args) const {
+  virtual bool execute_command(const std::string &name,
+                               basic_command::exec_args &&args) const {
     if (auto it = cmds_map_.find(name); it != cmds_map_.cend())
       return it->second->execute(std::move(args));
     return false;
   }
 
-  inline basic_completion::generator_func
+  basic_completion::generator_func
   find_param_generator(const std::string &name) const {
     if (auto it = cmds_map_.find(name); it != cmds_map_.cend())
       return std::bind(&basic_command::param_generator, it->second.get(),
